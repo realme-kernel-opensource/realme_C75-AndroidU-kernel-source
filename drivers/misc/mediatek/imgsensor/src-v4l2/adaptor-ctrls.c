6f610fc326cde11b108dda6804a181c9931997ee
mtk16547 <Zhuang.Wang@mediatek.com>
2023-05-08 15:49:05 +0800

    [ALPS07953321] cam:add adaptor trace
    [Solution] addn adaptor trace to debug
    
    MTK-Commit-Id: 41605af1e41f245c707d8c5f705bcbdc3252ca27
    
    Signed-off-by: mtk16547 <Zhuang.Wang@mediatek.com>
    CR-Id: ALPS07953321
    Feature: [Android Default] Camera Application Basic Functions
    Change-Id: Ic284c46bb2f9918284d9fc63ede0394a1bd29468

--- drivers/misc/mediatek/imgsensor/src-v4l2/adaptor-ctrls.c
+++ drivers/misc/mediatek/imgsensor/src-v4l2/adaptor-ctrls.c
@@ -11,6 +11,7 @@
 #include "adaptor-common-ctrl.h"
 #include "adaptor-fsync-ctrls.h"
 #include "adaptor-hw.h"
+#include "adaptor-trace.h"
 
 #define ctrl_to_ctx(ctrl) \
 	container_of(ctrl->handler, struct adaptor_ctx, ctrls)
@@ -192,14 +193,24 @@ static int do_set_ae_ctrl(struct adaptor_ctx *ctx,
 	switch (exp_count) {
 	case 3:
 	{
+		ADAPTOR_SYSTRACE_BEGIN("imgsensor::set_exposure_tri");
 		set_hdr_exposure_tri(ctx, &ae_ctrl->exposure);
+		ADAPTOR_SYSTRACE_END();
+
+		ADAPTOR_SYSTRACE_BEGIN("imgsensor::set_gain_tri");
 		set_hdr_gain_tri(ctx, &ae_ctrl->gain);
+		ADAPTOR_SYSTRACE_END();
 	}
 		break;
 	case 2:
 	{
+		ADAPTOR_SYSTRACE_BEGIN("imgsensor::set_exposure_dual");
 		set_hdr_exposure_dual(ctx, &ae_ctrl->exposure);
+		ADAPTOR_SYSTRACE_END();
+
+		ADAPTOR_SYSTRACE_BEGIN("imgsensor::set_gain_dual");
 		set_hdr_gain_dual(ctx, &ae_ctrl->gain);
+		ADAPTOR_SYSTRACE_END();
 	}
 		break;
 	case 1:
@@ -214,6 +225,7 @@ static int do_set_ae_ctrl(struct adaptor_ctx *ctx,
 						ae_ctrl->subsample_tags);
 		}
 
+		ADAPTOR_SYSTRACE_BEGIN("imgsensor::set_exposure");
 		fsync_exp[0] = ae_ctrl->exposure.le_exposure;
 		ret = chk_s_exp_with_fl_by_fsync_mgr(ctx, fsync_exp, 1);
 		if (!ret) {
@@ -224,24 +236,28 @@ static int do_set_ae_ctrl(struct adaptor_ctx *ctx,
 						para.u8, &len);
 		}
 		notify_fsync_mgr_set_shutter(ctx, fsync_exp, 1, ret);
-
-
+		ADAPTOR_SYSTRACE_END();
 
 		para.u64[0] = ae_ctrl->gain.le_gain;
 		para.u64[1] = 0;
 		para.u64[2] = 0;
+
+		ADAPTOR_SYSTRACE_BEGIN("imgsensor::set_gain");
 		subdrv_call(ctx, feature_control,
 					SENSOR_FEATURE_SET_GAIN,
 					para.u8, &len);
+		ADAPTOR_SYSTRACE_END();
 	}
 		break;
 	}
 
 	if (ae_ctrl->actions & IMGSENSOR_EXTEND_FRAME_LENGTH_TO_DOL) {
 		para.u64[0] = 0;
+		ADAPTOR_SYSTRACE_BEGIN("imgsensor::set_extend_frame_length");
 		subdrv_call(ctx, feature_control,
 					SENSOR_FEATURE_SET_SEAMLESS_EXTEND_FRAME_LENGTH,
 					para.u8, &len);
+		ADAPTOR_SYSTRACE_END();
 
 		notify_fsync_mgr_set_extend_framelength(ctx, para.u64[0]);
 	}
@@ -628,7 +644,7 @@ static int imgsensor_set_ctrl(struct v4l2_ctrl *ctrl)
 	 */
 	if (pm_runtime_get_if_in_use(dev) == 0)
 		return 0;
-
+	ADAPTOR_SYSTRACE_BEGIN("SensorWorker::%s %d", __func__, ctrl->id);
 	switch (ctrl->id) {
 	case V4L2_CID_UPDATE_SOF_CNT:
 		subdrv_call(ctx, update_sof_cnt, (u64)ctrl->val);
@@ -669,7 +685,19 @@ static int imgsensor_set_ctrl(struct v4l2_ctrl *ctrl)
 		}
 		break;
 	case V4L2_CID_MTK_STAGGER_AE_CTRL:
-		s_ae_ctrl(ctrl);
+		{
+			struct mtk_hdr_ae *ae_ctrl = ctrl->p_new.p;
+
+			ADAPTOR_SYSTRACE_BEGIN("SensorWorker::s_ae_ctrl %d %d %d %d %d %d",
+				ae_ctrl->req_id,
+				ae_ctrl->exposure.le_exposure,
+				ae_ctrl->exposure.me_exposure,
+				ae_ctrl->gain.le_gain,
+				ae_ctrl->gain.me_gain,
+				ctx->cur_mode->linetime_in_ns);
+			s_ae_ctrl(ctrl);
+			ADAPTOR_SYSTRACE_END();
+		}
 		break;
 	case V4L2_CID_EXPOSURE_ABSOLUTE:
 		{
@@ -1047,6 +1075,7 @@ static int imgsensor_set_ctrl(struct v4l2_ctrl *ctrl)
 		break;
 	}
 
+	ADAPTOR_SYSTRACE_END();
 	pm_runtime_put(dev);
 
 	return ret;
